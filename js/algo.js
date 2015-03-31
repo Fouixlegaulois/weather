7d9bef93c685dfc231d395cc148690a8e59bc167
Lyine <francois.colin73@gmail.com>
2015-03-31 20:11:00 +0200

    Add comments.

--- js/algo.js
+++ js/algo.js
@@ -1,16 +1,16 @@
+/*
+* skyTreatment : Draw the sky.
+*/
 function skyTreatment(temperature, clouds, sunrise, sunset, weather, context, width, height) {
 
     // Now
     var now = Math.floor(Date.now() / 1000);
 
-    console.log(now);
-    console.log(sunrise);
-    console.log(sunset);
-    // Draw sky
+    // Draw sky - define if this is the sun or the moon (test with sunset or sunrise)
     var assombrissement_pour_la_nuit = 0;
     if (now < sunrise || now > sunset) { assombrissement_pour_la_nuit = 50; }
 
-    
+    // assombrissement_pour_la_nuit : change a little the colors when it's the night
     var couleur_ciel_rouge = parseInt(100) - assombrissement_pour_la_nuit;
     var couleur_ciel_vert = parseInt(160 - (clouds / 1.7)) - assombrissement_pour_la_nuit;
     var couleur_ciel_bleu = parseInt(255 - (clouds * 1.6)) - assombrissement_pour_la_nuit;
@@ -29,7 +29,7 @@ function skyTreatment(temperature, clouds, sunrise, sunset, weather, context, wi
     // draw sun or moon
     drawSun(context, width, height, sun_or_moon_color);
 
-    // Define clouds 
+    // Define the image of the clouds 
     if (weather == 'light rain') {
         drawCloud(context, width, height, 'images/cloud_lightrain.png');
     } else if (weather == 'moderate rain' || weather == 'light intensity shower rain') {
@@ -56,6 +56,9 @@ function skyTreatment(temperature, clouds, sunrise, sunset, weather, context, wi
     
 }
 
+/*
+* groundTreatment : Draw the ground.
+*/
 function groundTreatment(temperature, clouds, sunrise, sunset, context, width, height) {
 
     // Now
@@ -65,10 +68,12 @@ function groundTreatment(temperature, clouds, sunrise, sunset, context, width, h
     var assombrissement_pour_la_nuit = 0;
     if (now < sunrise || now > sunset) { assombrissement_pour_la_nuit = 50; }
 
+    // assombrissement_pour_la_nuit : change a little the colors when it's the night
     var couleur_sol_rouge = 140 - assombrissement_pour_la_nuit;
     var couleur_sol_vert = 200 - assombrissement_pour_la_nuit;
     var couleur_sol_bleu = 100 - assombrissement_pour_la_nuit;
     
+    // Change ground color with temperature
     // Si température < 0 celsius => blanc comme neige
     if (temperature <= 272) {
         couleur_sol_rouge = 255 - assombrissement_pour_la_nuit;
@@ -87,7 +92,8 @@ function groundTreatment(temperature, clouds, sunrise, sunset, context, width, h
         var couleur_sol_vert = 200 - assombrissement_pour_la_nuit;
         var couleur_sol_bleu = 100 - assombrissement_pour_la_nuit;
     }
-          
+       
+    // Draw ground   
     rgb_sol = 'rgb('+couleur_sol_rouge+', '+couleur_sol_vert+', '+couleur_sol_bleu+')';  
     drawSol(context, rgb_sol, width, height);
 
@@ -95,8 +101,12 @@ function groundTreatment(temperature, clouds, sunrise, sunset, context, width, h
 
 }
 
+/*
+* seaTreatment : Draw the sea.
+*/
 function seaTreatment(temperature, clouds, sunrise, sunset, wind, context, width, height) {
 
+    // Change the sea image with the wind
     if (wind < 5) {
         drawSea(context, width, height, 'images/mer_calme.png')
     } else if (wind < 10) {
@@ -111,8 +121,13 @@ function seaTreatment(temperature, clouds, sunrise, sunset, wind, context, width
     
 }
 
+/*
+* setInformations : draw the informations about the weather and the location.
+*/
 function setInformations(temperature, clouds, sunrise, sunset, wind, weather, station, x) {
+    // Set the temperature in celsius
     var temperature_celsius = temperature - 272.15;
+    // Draw the informations
     x.innerHTML = '<span class="small">Weather station : </span><span class="big">' + station + '</span><br>';
     x.innerHTML += '<span class="small">Weather : </span><span class="big">' + weather + '</span><span class="small"></span><br>';
     x.innerHTML += '<span class="small">Clouds : </span><span class="big">' + clouds + '</span><span class="small"> %</span><br>';
@@ -120,7 +135,11 @@ function setInformations(temperature, clouds, sunrise, sunset, wind, weather, st
     x.innerHTML += '<span class="small">Wind : </span><span class="big">' + wind + '</span><span class="small"> m/s</span><br>';
 }
 
+/*
+* montainTreatment : draw the mountain.
+*/
 function montainTreatment(temperature, context, width, height) {
+    // Change the mountain image with the temperature
     var image = 'images/montagne_ete.png';
     if (temperature <= 272) {
         image = 'images/montagne_neigeplusplus.png';
--- js/drawing.js
+++ js/drawing.js
@@ -1,19 +1,51 @@
+/*
+* Sky:
+* Params: 
+*   -context (for the canvas)
+*   -rgb_ciel (sky color)
+*   -width 
+*   -height
+*/
 function drawSky(context, rgb_ciel, width, height) {
     context.fillStyle = rgb_ciel;
     context.fillRect(0, 0, width, height / 2);
 }
 
+/*
+* Ground:
+* Params: 
+*   -context (for the canvas)
+*   -rgb_sol (ground color)
+*   -width 
+*   -height
+*/
 function drawSol(context, rgb_sol, width, height) {
     context.fillStyle = rgb_sol;
     context.fillRect(0, height / 2, width, height);
 }
 
+/*
+* Sun OR moon:
+* Params: 
+*   -context (for the canvas)
+*   -color (define if this is the sun or the moon)
+*   -width 
+*   -height
+*/
 function drawSun(context, width, height, color) {
     context.arc(width - 400, height / 4, 70, 0, 2 * Math.PI, false);
     context.fillStyle = color;
     context.fill();
 }
 
+/*
+* Clouds:
+* Params: 
+*   -context (for the canvas)
+*   -image (define the cloud image)
+*   -width 
+*   -height
+*/
 function drawCloud(context, width, height, image) {
     var img = new Image();
     img.src = image;
@@ -22,6 +54,14 @@ function drawCloud(context, width, height, image) {
     }
 }
 
+/*
+* Sea:
+* Params: 
+*   -context (for the canvas)
+*   -image (define the sea image)
+*   -width 
+*   -height
+*/
 function drawSea(context, width, height, image) {
     var img = new Image();
     img.src = image;
@@ -30,14 +70,14 @@ function drawSea(context, width, height, image) {
     }
 }
 
-function drawPalmier(context, width, height) {
-    /*var img = new Image();
-    img.src = 'images/palmier.png';
-    img.onload = function(){
-        context.drawImage(img, width / 3, height/3.5);
-    }*/
-}
-
+/*
+* Mountain:
+* Params: 
+*   -context (for the canvas)
+*   -image (define the mountain image)
+*   -width 
+*   -height
+*/
 function drawMountain(context, width, height, image) {
     var img = new Image();
     img.src = image;
